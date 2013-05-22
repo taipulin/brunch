3b56156464416a0735056c6128b49ca24f5cba06
taipulin <taipulin@gmail.com>
2013-05-22 00:59:02 -0700

    delete description

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Brunch::Application.configure do
    # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
-  Paperclip.options[:command_path] = "/usr/bin"
+  # Paperclip.options[:command_path] = "/usr/bin"
 
     config.paperclip_defaults = {
     :storage => :s3,
